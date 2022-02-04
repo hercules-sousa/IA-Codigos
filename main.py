5b510ebad23d2b9eabd8fd04f78e70be73a0aa30
Hercules de Sousa Silva <hercules.sousa@academico.ifpb.edu.br>
2022-02-04 14:22:19 -0300

    Removendo código desnecessário

--- main.py
+++ main.py
@@ -1,53 +1,6 @@
-from grafo import Grafo
-from leitores.leitorTxt import LeitorTxt
-from mapaEstado import MapaEstado
 import os
-
-'''
-grafo1 = Grafo(list("ABCD"), [
-    ["A-B", 12],
-    ["B-C", 15],
-    ["A-C", 1],
-    ["C-D", 2]
-])
-
-print(grafo1.busca_em_profundidade("A", "C"))
-print(grafo1.busca_em_largura("A", "C"))
-
-grafo2 = Grafo(list("ABCD"), [
-    ["A-B", 4],
-    ["A-C", 1],
-    ["C-D", 2],
-])
-
-print(grafo2.busca_em_profundidade("A", "D"))
-print(grafo2.busca_em_largura("A", "D"))
-
-grafo3 = Grafo(list("ABC"), [
-    ["A-A", 12],
-    ["A-B", 4],
-    ["B-C", 5],
-])
-
-print(grafo3.busca_em_profundidade("A", "C"))
-print(grafo3.busca_em_largura("A", "C"))
-print(grafo3.busca_em_profundidade("C", "A"))
-print(grafo3.busca_em_largura("C", "A"))
-
-grafo4 = Grafo(list("ABCDEFG"), [
-    ["A-A", 10],
-    ["A-B", 4],
-    ["B-B", 7],
-    ["B-C", 1],
-    ["B-D", 8],
-    ["D-E", 9],
-    ["E-F", 5],
-    ["E-G", 4],
-])
-print(grafo4.busca_em_largura("C", "G"))
-
-print(grafo4.busca_em_profundidade("C", "G"))
-'''
+from mapaEstado import MapaEstado
+from leitores.leitorTxt import LeitorTxt
 
 nome_pasta = os.path.dirname(__file__)
 caminho_txt = os.path.join(nome_pasta, 'arquivos/paraiba.txt')
@@ -60,7 +13,11 @@ with open(caminho_txt) as arquivo:
 
     print("Caminho da busca em profundidade")
     print(mapaEstado.mapaGrafo.busca_em_profundidade("Campina Grande", "Montadas"))
+    print()
+    print("Caminho da busca em largura")
     print(mapaEstado.mapaGrafo.busca_em_largura("Campina Grande", "Montadas"))
+    print()
+    print("Caminho da busca em estrela")
     print(mapaEstado.mapaGrafo.busca_a_estrela("Campina Grande", "Montadas", "Campina Grande"))
 
 arquivo.close()
